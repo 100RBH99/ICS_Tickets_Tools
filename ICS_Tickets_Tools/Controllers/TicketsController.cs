550e999862ae139283eb2a0fd3597e3cf3f07cc8
ICS489 <111403@ICSPC42>
2025-08-12 15:27:02 +0530

    tickets controler 12/08/2025

--- ICS_Tickets_Tools/Controllers/TicketsController.cs
+++ ICS_Tickets_Tools/Controllers/TicketsController.cs
@@ -22,7 +22,7 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
  
-namespace ICS_Tickets_Tools.Controllers //vvvvviiojijiirgfgfg
+namespace ICS_Tickets_Tools.Controllers //vvvvvghghhgghiiojijiirgfgfgjvhg
 {
 	[Authorize]
 	public class TicketsController : Controller
