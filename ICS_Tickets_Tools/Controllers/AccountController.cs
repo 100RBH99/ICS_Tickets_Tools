2d1b402d7147e81b01348d189d990680b91faafd
100RBH99 <sourabhdubey1151@gmail.com>
2025-09-10 11:52:32 +0530

    rinku commit

--- ICS_Tickets_Tools/Controllers/AccountController.cs
+++ ICS_Tickets_Tools/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ using ICS_Tickets_Tools.Repositories;
 
 namespace ICS_Tickets_Tools.Controllers
 {
-    public class AccountController : Controller//rinkuu
+    public class AccountController : Controller//rinkuuuuydd
     {
         private readonly IAccountRepository _accountRepo;
 		private readonly RoleManager<IdentityRole> _roleManager;
