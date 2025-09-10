e19be2ff07b4fbd56020b75281cd19e69044b204
100RBH99 <sourabhdubey1151@gmail.com>
2025-09-10 12:01:10 +0530

    dddd

--- ICS_Tickets_Tools/Controllers/AccountController.cs
+++ ICS_Tickets_Tools/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ using ICS_Tickets_Tools.Repositories;
 
 namespace ICS_Tickets_Tools.Controllers
 {
-    public class AccountController : Controller//rinkuuuuyddf
+    public class AccountController : Controller//rinkuuuuyddfss
     {
         private readonly IAccountRepository _accountRepo;
 		private readonly RoleManager<IdentityRole> _roleManager;
